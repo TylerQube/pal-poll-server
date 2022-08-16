e154fc7d0fbe8a4114bc429c321cda91545eb3ca
BuildTools <tylersmith1308@gmail.com>
2022-08-16 17:34:08 -0500

    Authorize with jwt secret env variable

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 exports.authUser = (req, res, next) => {
   try {
     const token = req.headers.authorization.replace("Bearer ", "");
-    const decoded = jwt.verify(token, "secret");
+    console.log("Secret: " + process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET ?? "secret");
     // save decoded user data to request for later use 
     req.userData = decoded;
     console.log("User authorized");
