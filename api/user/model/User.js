2914861df3c85f5cf427bf74d3eed1a608ada023
BuildTools <tylersmith1308@gmail.com>
2022-08-16 14:39:06 -0500

    Add salt environment template.

--- api/user/model/User.js
+++ api/user/model/User.js
@@ -35,7 +35,7 @@ const userSchema = mongoose.Schema({
 userSchema.pre("save", async function(next) {
   const user = this; 
   if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8);
+    user.password = await bcrypt.hash(user.password, process.env.SALT);
   }
   next();
 });
